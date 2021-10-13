e1b5ae9faaf9d714f136f31d272f3071610e8ae7
lkshrsch <lukashirsch@gmail.com>
2021-10-13 13:43:05 -0400

    shell instructions

---
+++ run_training_and_testing.sh
@@ -0,0 +1,5 @@
+
+python TRAIN_TEST.py /home/Segmentation_breast_cancer/configFiles/configFile_Training.py;
+
+python SEGMENT.py    /home/Segmentation_breast_cancer/configFiles/configFile_Testing_Prediction.py;
+
